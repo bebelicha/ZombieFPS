ce94811631c4841e669f8dc1598237ccb9589635
Isabela Fernandes <isabelafgm@gmail.com>
2022-08-19 01:16:49 -0300

    feat: project

---
+++ ZombieFPS/Assets/Scripts/MovimentoJogador.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoJogador : MovimentoPersonagem
+{
+     public void RotacaoJogador()
+    {
+        Vector3 posicaoMiraJogador = this.Direcao;
+        posicaoMiraJogador.y = transform.position.y;
+        Rotacionar(posicaoMiraJogador);
+    }
+}
+
